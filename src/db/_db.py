e96c1cbbe5bcd11a8e4a1686bb7ebdc3ddf8dee5
knb <craneheart3@gmail.com>
2026-03-07 06:13:13 +0800

    fix:对于测试使用的sqlite进行完善

--- src/db/_db.py
+++ src/db/_db.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+from sqlalchemy import event
 from fastapi import Depends
 from sqlmodel import create_engine, Session
 
@@ -7,6 +8,13 @@ from core import get_settings
 
 settings = get_settings()
 engine = create_engine(settings.db_url)
+
+if settings.db_url.startswith("sqlite"):
+    @event.listens_for(engine, "connect")
+    def _set_sqlite_pragma(dbapi_connection, _connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 
 def get_session():

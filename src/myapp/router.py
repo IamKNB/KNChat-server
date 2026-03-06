3e2d0e867a6f2854376ca607290e038c9db2b6b7
knb <craneheart3@gmail.com>
2026-03-07 03:27:41 +0800

    初始化chat modules

--- src/myapp/router.py
+++ src/myapp/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from myapp.modules import chat
 from myapp.modules import profiles
 
 router = APIRouter(prefix="/api", )
 
 router.include_router(profiles.api_router)
+router.include_router(chat.api_router)

1840bdefdbe53d122905dd8ee565b2ff45a8b809
knb <craneheart3@gmail.com>
2026-03-09 02:01:46 +0800

    sqlite支持,使用sqlmodel

--- src/db/_db.py
+++ src/db/_db.py
@@ -1,24 +1,18 @@
 from typing import Annotated
 
-from sqlalchemy import event
 from fastapi import Depends
-from sqlmodel import create_engine, Session
+from sqlmodel import Session, create_engine
 
 from core import get_settings
 
 settings = get_settings()
 engine = create_engine(settings.db_url)
 
-if settings.db_url.startswith("sqlite"):
-    @event.listens_for(engine, "connect")
-    def _set_sqlite_pragma(dbapi_connection, _connection_record):
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
-
 
 def get_session():
     with Session(engine) as session:
+        if settings.db_url.startswith("sqlite"):
+            session.connection().exec_driver_sql("PRAGMA foreign_keys=ON")
         yield session
 
 
